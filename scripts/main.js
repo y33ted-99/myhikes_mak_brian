5101b450fcd405e84b19e586aa3303c43e37a334
Brian Mak <brianmak.blue@gmail.com>
2022-03-01 17:25:44 -0800

    changes for part 1,2,3

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,25 @@
-function read_display_Quote() {
-    db.collection("quotes").doc("tuesday")                                                     
-      .onSnapshot(tuesdayDoc => {                                                              
-           console.log(tuesdayDoc.data());                        
-           document.getElementById("quote-goes-here").innerHTML = tuesdayDoc.data().quote;     
-           
-          
+function read_display_Quote(){
+
+    db.collection("something").doc("monday")                                                     
+      .onSnapshot(function(mondayDoc){                                                              
+           console.log(mondayDoc.data());                        
+           document.getElementById("quote-goes-here").innerHTML = mondayDoc.data().quote;        
       })
 }
-read_display_Quote()        //calling the function
+read_display_Quote()       
+
+function insertName(){
+    firebase.auth().onAuthStatChanged(user =>{
+         if(user){
+              console.log(user.uid);
+              currentUser = db.collection("users").doc(user.uid);
+              currentUser.get().then(userDoc=>{
+                   var user_Name= userDoc.data().name;
+                   console.log(user_Name);
+                   $("#name-goes-here").text(user_Name);
+              })
+         }
+    })
+
+}
+insertName();
